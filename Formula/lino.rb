d13a9e72266b2738ddb8e48a9311efd7b9f2e1a3
Gambhir ⚡ <gambhir786191ss@gmail.com>
2025-08-22 13:18:24 +0200

    chore: rm 32 bit linux binary
    Homebrew don't support 32bit linux binary

--- Formula/lino.rb
+++ Formula/lino.rb
@@ -8,15 +8,8 @@ class Lino < Formula
     url "https://github.com/CGI-FR/LINO/releases/download/v#{version}/LINO_#{version}_darwin_amd64.tar.gz"
     sha256 "04d1f7515d8e754f2614faef63cb289b9a3107140df52284ae79d0fb18e41351"
   elsif OS.linux? 
-    if Hardware::CPU.intel?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/CGI-FR/LINO/releases/download/v#{version}/LINO_#{version}_linux_amd64.tar.gz"
-        sha256 "3939db3e66219b0a1e002abf822443fed6261e67c1b0d031112d2db8009ef856"
-      else
-        url "https://github.com/CGI-FR/LINO/releases/download/v#{version}/LINO_#{version}_linux_386.tar.gz"
-        sha256 "e1b37706c8fffa51b663257012b7d9a123272a19beffe39dfb2911528a2bf066"
-      end
-    end
+    url "https://github.com/CGI-FR/LINO/releases/download/v#{version}/LINO_#{version}_linux_amd64.tar.gz"
+    sha256 "3939db3e66219b0a1e002abf822443fed6261e67c1b0d031112d2db8009ef856"
   end
 
   def install
